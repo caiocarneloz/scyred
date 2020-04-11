754e5292201f271fb60cd2b4acd4bbd30cd45024
caiocarneloz <caiocarneloz@gmail.com>
2020-04-11 01:57:49 -0300

    package

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from algorithms import ga_opt
+from algorithms import pso_opt
+from core import scyred
+
+__all__ = ['scyred']
+__version__ = '0.0.3'
+__author__ = 'Caio Carneloz <caiocarneloz@gmail.com>'
+__url__ = 'https://github.com/caiocarneloz/scyred'
